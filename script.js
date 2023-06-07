ffdc1959e09a887423bdc1e02a92ec1d9288d214
ctokoli <matumasie@gamil.com>
2023-06-07 09:33:15 +0000

    add linters configuration files

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 const button = document.querySelector('button');
-let title = document.querySelector('.title');
-let author = document.querySelector('.author');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
 const content = document.querySelector('.books');
 
-let userData = JSON.parse(localStorage.getItem('formdata')).reverse();
+const userData = JSON.parse(localStorage.getItem('formdata')).reverse();
 let count = userData.length;
-console.log(userData);
-console.log(count);
-
 
 function deleteItem(author) {
-    let filteredArray = userData.filter( e => e.author !== author)
-    console.log(filteredArray);
+  const filteredArray = userData.filter((e) => e.author !== author);
   localStorage.setItem('formdata', JSON.stringify(filteredArray));
 }
 
 function fetchBooks() {
-        let placeholder = '';
-        userData.forEach((book, index) => {
-            placeholder += `
+  let placeholder = '';
+  userData.forEach((book) => {
+    placeholder += `
                 <div>
                     <h3>${book.title}</h3>
                     <p>${book.author}</p>
@@ -26,30 +22,21 @@ function fetchBooks() {
                 <button onclick="deleteItem('${book.author}')">Remove</button>
                 <hr>
             `;
+  });
+  content.innerHTML = placeholder;
 
-        }); 
-        content.innerHTML = placeholder;
-
-        button.addEventListener('click', (e) => {
-            e.preventDefault();
-            let formData = {
-                id: count +=1,
-                title: title.value,
-                author: author.value
-            };
-            //console.log(userData);
-            userData.push(formData);
-            //console.log(userData);
-            localStorage.setItem('formdata', JSON.stringify(userData));
-        });
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const formData = {
+      id: count += 1,
+      title: title.value,
+      author: author.value,
+    };
+    // console.log(userData);
+    userData.push(formData);
+    // console.log(userData);
+    localStorage.setItem('formdata', JSON.stringify(userData));
+  });
 }
 
 fetchBooks();
- 
-
- 
-
-
-
-
-
