502c046147e58d251069f3d905979e09f087fefa
ctokoli <matumasie@gamil.com>
2023-06-09 10:27:17 +0000

    styled the website

--- script.js
+++ script.js
@@ -44,14 +44,16 @@ class BookManager {
 
   renderBooks() {
     let placeholder = '';
-    this.userData.forEach((book) => {
+    this.userData.forEach((book, index) => {
       const backgroundColor = index % 2 === 0 ? 'white' : 'grey';
       placeholder += `
-        <div class="book" style="background-color: ${backgroundColor};">
-          <h3>"${book.title}" by ${book.author}</h3>
+       <div class="book-item" style="background-color: ${backgroundColor};">
+        <div class="book" >
+        <h3>"${book.title}" by ${book.author}</h3>
         </div>
         <button class="remove-btn" data-id="${book.id}">Remove</button>
-        <hr>
+       </div>
+  
       `;
     });
     this.content.innerHTML = placeholder;
